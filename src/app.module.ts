af69bbb847f8b7ebbe50f8d896b37ab679454092
Sara Al Arab <sara.alarab@lau.edu>
2023-02-16 17:06:31 +0200

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { BranchController } from './branch/branch.controller';
+import { BranchModule } from './branch/branch.module';
+import { BranchService } from './branch/branch.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule, BranchModule],
+  controllers: [BranchController],
+  providers: [BranchService],
 })
 export class AppModule {}
