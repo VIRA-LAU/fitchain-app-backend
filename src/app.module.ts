8dd0d0c52b3480f1ebc6f571b549be3e50ae2830
Sara Al Arab <sara.alarab@lau.edu>
2023-02-16 17:12:11 +0200

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { CourtModule } from './court/court.module';
+import { CourtController } from './court/court.controller';
+import { CourtService } from './court/court.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule, CourtModule],
+  controllers: [CourtController],
+  providers: [CourtService, ],
 })
 export class AppModule {}
