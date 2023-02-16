1603ef85b44d3c4bd8c987469241cfeac6c61247
Sara Al Arab <sara.alarab@lau.edu>
2023-02-16 17:14:43 +0200

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { VenueModule } from './venue/venue.module';
+import { VenueController } from './venue/venue.controller';
+import { VenueService } from './venue/venue.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UserModule, PrismaModule, VenueModule],
+  controllers: [VenueController],
+  providers: [VenueService],
 })
 export class AppModule {}
