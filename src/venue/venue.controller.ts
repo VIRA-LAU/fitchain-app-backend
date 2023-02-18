cec98cafec2c7fb7499cfeab41dc38dca60a6314
Sara Al Arab <sara.alarab@lau.edu>
2023-02-18 16:40:06 +0200

    add service

--- src/venue/venue.controller.ts
+++ src/venue/venue.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GetVenues } from 'src/auth/decorator';
+import { VenueService } from './venue.service';
 
-@Controller('venue')
-export class VenueController {}
+@Controller('venues')
+export class VenueController {
+    constructor(private venueService: VenueService){}
+
+    @Get()
+    getVenues(){
+        return this.venueService.getVenues()
+    }
+}
--- src/venue/venue.service.ts
+++ src/venue/venue.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class VenueService {}
+export class VenueService {
+    constructor(private prisma: PrismaService){}
+
+    async getVenues(){
+        const venues = await this.prisma.venue.findMany()
+
+        return venues;
+    }
+}
