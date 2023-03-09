da2ad34b0c345c71cdc6f7d65c6dc3069fe1c0d8
Khaled Jalloul <khaled.jalloul@hotmail.com>
2023-03-09 14:00:06 +0200

    Fix create game

--- src/game/dto/create-booking.dto.ts
+++ src/game/dto/create-booking.dto.ts
@@ -1,6 +1,11 @@
+import { gameType } from "@prisma/client"
 import { IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator"
 
 export class createBookingDto{
+
+    @IsString()
+    @IsNotEmpty()
+    type: gameType
 
     @IsNumber()
     @IsNotEmpty()
