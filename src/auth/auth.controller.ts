d26a523b0fa691023f1779db0421f927e01b46ef
Khaled Jalloul <khaled.jalloul@hotmail.com>
2023-07-20 02:04:27 +0300

    Create html templates and endpoints for password reset

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,39 +1,79 @@
-import { Body, Controller, HttpCode, HttpStatus, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthSigninDto, AuthSignupDto, BranchAuthSignupDto, ResendEmailCodeDto, VerifyBranchEmailDto, VerifyEmailDto } from './dto';
+import {
+  AuthSigninDto,
+  AuthSignupDto,
+  BranchAuthSignupDto,
+  ResendEmailCodeDto,
+  VerifyBranchEmailDto,
+  VerifyEmailDto,
+} from './dto';
+import * as path from 'path';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
-    
-    @Post('signup/user')
-    signupAsUser(@Body() dto: AuthSignupDto) {
-        return this.authService.signupAsUser(dto);
-    }
+  constructor(private authService: AuthService) {}
 
-    @Post('signup/branch')
-    signupAsBranch(@Body() dto: BranchAuthSignupDto) {
-        return this.authService.signupAsBranch(dto);
-    }
+  @Post('signup/user')
+  signupAsUser(@Body() dto: AuthSignupDto) {
+    return this.authService.signupAsUser(dto);
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Post('signin')
-    signin(@Body() dto: AuthSigninDto){
-        return this.authService.signin(dto);
-    }
-    
-    @Patch('verifyEmail/user')
-    verifyUserEmail(@Body() dto: VerifyEmailDto) {
-        return this.authService.verifyUserEmail(dto.userId, dto.code);
-    }
-    
-    @Patch('verifyEmail/branch')
-    verifyBranchEmail(@Body() dto: VerifyBranchEmailDto) {
-        return this.authService.verifyBranchEmail(dto.branchId, dto.code);
-    }
+  @Post('signup/branch')
+  signupAsBranch(@Body() dto: BranchAuthSignupDto) {
+    return this.authService.signupAsBranch(dto);
+  }
 
-    @Patch('resendEmailCode')
-    resendEmailCode(@Body() dto: ResendEmailCodeDto) {
-        return this.authService.resendEmailCode(dto.userId, dto.isBranch);
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: AuthSigninDto) {
+    return this.authService.signin(dto);
+  }
+
+  @Patch('verifyEmail/user')
+  verifyUserEmail(@Body() dto: VerifyEmailDto) {
+    return this.authService.verifyUserEmail(dto.userId, dto.code);
+  }
+
+  @Patch('verifyEmail/branch')
+  verifyBranchEmail(@Body() dto: VerifyBranchEmailDto) {
+    return this.authService.verifyBranchEmail(dto.branchId, dto.code);
+  }
+
+  @Patch('resendEmailCode')
+  resendEmailCode(@Body() dto: ResendEmailCodeDto) {
+    return this.authService.resendEmailCode(dto.userId, dto.isBranch);
+  }
+
+  @Patch('forgotPassword')
+  forgotPassword(@Body() dto: { email: string }) {
+    // this.authService.
+  }
+
+  @Get('resetPassword')
+  resetPassword(@Res() res: Response) {
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, './html-templates/password-reset.html'));
+  }
+
+  @Patch('updatePassword')
+  updatePassword(
+    @Body() dto: { token: string; password: string },
+    @Res() res: Response,
+  ) {
+    console.log(dto);
+    res.status(200).json(dto);
+  }
 }
